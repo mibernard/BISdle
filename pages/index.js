cf91c52e176da80be1a68771eaed8b059a3beab8
Matthew <matthewbernard4@gmail.com>
2024-05-07 15:40:25 -0700

    add item mapping

--- pages/index.js
+++ pages/index.js
@@ -326,7 +326,7 @@ export default function Home() {
     InkshadowOx: 'TattooOfVitality',
     InkshadowPig: 'TattooOfProtection',
     InkshadowSnake: 'TattooOfToxin',
-    Moonstone: '',
+    Moonstone: 'MoonstoneRenewer',
     SpectralGauntlet: '',
     ForceOfNature: 'TacticiansCrown',
   };
