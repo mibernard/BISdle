3857972c857cf22fdc172cb5e2810b2ebe10be7f
Matthew Bernard <matthewbernard4@gmail.com>
2025-10-15 15:01:42 -0700

    Update _document.js and globals.css for improved layout and styling on mobile devices

--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,15 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-        <link rel="icon" href="/favicon.png?v=2" />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+          <link rel='icon' href='/favicon.png?v=2' />
           {/* Google AdSense */}
-          <script 
-            async 
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8082087458028817"
-            crossOrigin="anonymous"
+          <script
+            async
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8082087458028817'
+            crossOrigin='anonymous'
           />
         </Head>
         <body>
@@ -23,4 +24,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
